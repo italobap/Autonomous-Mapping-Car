b84bfeb02aa5d23f5313c954c1fd58e3ad33ddae
italobap <48602485+italobap@users.noreply.github.com>
2022-10-03 21:22:44 -0300

    Código movimentação carro

---
+++ Autonomous_Mapping_Car.ino
@@ -0,0 +1,142 @@
+#include <dummy.h>
+#include <NewPing.h>        //Ultrasonic sensor function library. You must install this library
+
+//our L298N control pins
+const int LeftMotorForward = 7;
+const int LeftMotorBackward = 6;
+const int RightMotorForward = 4;
+const int RightMotorBackward = 5;
+
+//sensor pins
+#define trig_pin A1 //analog input 1
+#define echo_pin A2 //analog input 2
+
+#define maximum_distance 200
+boolean goesForward = false;
+int distance = 100;
+
+NewPing sonar(trig_pin, echo_pin, maximum_distance); //sensor function
+
+void setup(){
+
+  pinMode(RightMotorForward, OUTPUT);
+  pinMode(LeftMotorForward, OUTPUT);
+  pinMode(LeftMotorBackward, OUTPUT);
+  pinMode(RightMotorBackward, OUTPUT);
+
+
+}
+
+int readPing(){
+  delay(70);
+  int cm = sonar.ping_cm();
+  if (cm==0){
+    cm=250;
+  }
+  return cm;
+}
+
+void moveStop(){
+  
+  digitalWrite(RightMotorForward, LOW);
+  digitalWrite(LeftMotorForward, LOW);
+  digitalWrite(RightMotorBackward, LOW);
+  digitalWrite(LeftMotorBackward, LOW);
+}
+
+void moveForward(){
+
+  if(!goesForward){
+
+    goesForward=true;
+    
+    digitalWrite(LeftMotorForward, HIGH);
+    digitalWrite(RightMotorForward, HIGH);
+  
+    digitalWrite(LeftMotorBackward, LOW);
+    digitalWrite(RightMotorBackward, LOW); 
+  }
+}
+
+void moveBackward(){
+
+  goesForward=false;
+
+  digitalWrite(LeftMotorBackward, HIGH);
+  digitalWrite(RightMotorBackward, HIGH);
+  
+  digitalWrite(LeftMotorForward, LOW);
+  digitalWrite(RightMotorForward, LOW);
+  
+}
+
+void turnRight(){
+
+  digitalWrite(LeftMotorForward, HIGH);
+  digitalWrite(RightMotorBackward, HIGH);
+  
+  digitalWrite(LeftMotorBackward, LOW);
+  digitalWrite(RightMotorForward, LOW);
+  
+  delay(500);
+  
+  digitalWrite(LeftMotorForward, HIGH);
+  digitalWrite(RightMotorForward, HIGH);
+  
+  digitalWrite(LeftMotorBackward, LOW);
+  digitalWrite(RightMotorBackward, LOW);
+ 
+  
+  
+}
+
+void turnLeft(){
+
+  digitalWrite(LeftMotorBackward, HIGH);
+  digitalWrite(RightMotorForward, HIGH);
+  
+  digitalWrite(LeftMotorForward, LOW);
+  digitalWrite(RightMotorBackward, LOW);
+
+  delay(500);
+  
+  digitalWrite(LeftMotorForward, HIGH);
+  digitalWrite(RightMotorForward, HIGH);
+  
+  digitalWrite(LeftMotorBackward, LOW);
+  digitalWrite(RightMotorBackward, LOW);
+}
+
+void loop(){
+
+  int distanceRight = 0;
+  int distanceLeft = 0;
+  delay(50);
+  moveForward(); 
+
+  distance = readPing();
+  delay(100);
+
+  if (distance <= 20){
+    moveStop();
+    delay(300);
+    moveBackward();
+    delay(400);
+    moveStop();
+    delay(300);
+
+    if (distance >= distanceLeft){
+      turnRight();
+      moveStop();
+    }
+    else{
+      turnLeft();
+      moveStop();
+    }
+  }
+  else{
+    moveForward(); 
+  }
+    distance = readPing();
+    Serial.print(distance);
+}
